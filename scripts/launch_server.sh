b3a15997d462aefb5726169a672e27cfed66df04
Saúl Fernández <sftobias@ifca.unican.es>
2024-01-30 13:26:23 +0100

    chore: install exim4 server

--- scripts/launch_server.sh
+++ scripts/launch_server.sh
@@ -10,5 +10,5 @@ cd "$script_dir/../server/"
 docker kill mail-server
 docker remove mail-server
 docker build -t mail-server_image .
-docker run --name mail-server --env-file vars.env --network mailing_net --ip 192.168.1.3 -p 0.0.0.0:8082:8082 -d mail-server_image:latest
+docker run --name mail-server --env-file vars.env --network mailing_net --ip 192.168.1.3 -p 0.0.0.0:8082:8082 -v /etc/exim4:/etc/exim4 -d mail-server_image:latest
 docker container logs mail-server
