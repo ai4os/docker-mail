b02d8626bb6a3338be72c95709e3e4e41b9b081a
Saúl Fernández <sftobias@ifca.unican.es>
2024-01-30 14:36:12 +0100

    style: fixed python style

--- client/mail_client.py
+++ client/mail_client.py
@@ -1,9 +1,11 @@
 import os
 import requests
+
+
 from dateutil import parser
 
+
 def send_mail(email: str, body: str, subject: str, token: str):
-    
     api_url = "http://192.168.1.3:8082/notify"
     params = {"email": email, "body": body, "subject": subject, "token": token}
 
@@ -11,25 +13,25 @@ def send_mail(email: str, body: str, subject: str, token: str):
     data = response.json()
     print(data)
 
-def get_current_date():
 
+def get_current_date():
     global current_date
     url = "http://worldtimeapi.org/api/timezone/Europe/Madrid"
     response = requests.get(url)
     data = response.json()
-    current_date = parser.parse(data['datetime'])
-    
+    current_date = parser.parse(data["datetime"])
+
     return current_date
 
-def compare_date(date):
 
+def compare_date(date):
     current_date = get_current_date()
     diff = current_date - date
-    
+
     return diff.days > int(os.getenv("NUM_DAYS", 7))
 
-def main():
 
+def main():
     date = parser.parse(os.getenv("DATE").strip('"'))
     mail = os.getenv("DEST")
     body = os.getenv("BODY").strip('"')
@@ -38,5 +40,6 @@ def main():
 
     if compare_date(date):
         send_mail(mail, body, subject, token)
+
 
 main()
--- server/mail_server.py
+++ server/mail_server.py
@@ -1,52 +1,62 @@
+import os
+import subprocess
+
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 
-import os
-import subprocess
 
 token_env_name = "MAILING_TOKEN"
 token_val = os.getenv(token_env_name)
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 async def status():
-    return {'message': 'server is UP'}
+    return {"message": "server is UP"}
+
 
 @app.get("/notify")
 def notify(email: str, body: str, subject: str, token: str):
-
     if token == token_val:
         send_mail(email, body, subject)
         return {"status": "ok", "message": "Notification sent successfully"}
     else:
         return {"status": "error", "message": "Invalid token, notification not sent"}
-    
+
+
 def send_mail(email: str, body: str, subject: str):
-  
-    print(email, body, subject);
+    print(email, body, subject)
 
     bash_command = f"echo '{body}' | mail -s '{subject}' {email}"
 
     try:
-        subprocess.run(bash_command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        subprocess.run(
+            bash_command,
+            check=True,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
     except subprocess.CalledProcessError as e:
         print(e.stderr)
 
+
 def custom_openapi():
-    
     if app.openapi_schema:
         return app.openapi_schema
-    
+
     openapi_schema = get_openapi(
         title="Mail-Service",
         version="1.0.0",
         description="Service for sending mails",
         routes=app.routes,
     )
-    
+
     app.openapi_schema = openapi_schema
-    
+
     return app.openapi_schema
+
 
 app.openapi = custom_openapi
