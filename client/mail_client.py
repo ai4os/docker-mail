c7ba9749461cf9867eeedfbd3a0befb0a526feea
Saúl Fernández <sftobias@ifca.unican.es>
2024-04-09 12:44:26 +0200

    chore: date refactor

--- client/mail_client.py
+++ client/mail_client.py
@@ -1,9 +1,6 @@
 import os
 import requests
-
-
-from dateutil import parser
-
+import datetime
 
 def send_mail(email: str, body: str, subject: str, token: str):
     api_url = "https://api.cloud.ai4eosc.eu:5000/notify"
@@ -14,26 +11,20 @@ def send_mail(email: str, body: str, subject: str, token: str):
     print(data)
 
 
-def get_current_date():
-    global current_date
-    url = "http://worldtimeapi.org/api/timezone/Europe/Madrid"
-    response = requests.get(url)
-    data = response.json()
-    current_date = parser.parse(data["datetime"])
-
-    return current_date
-
-
 def compare_date(date):
-    current_date = get_current_date()
+    
+    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    current_date = datetime.date.today()
+    
     diff = current_date - date
 
+    print(diff.days)
     return diff.days > int(os.getenv("NUM_DAYS", 7).strip('"'))
 
 
 def main():
     try:
-        date = parser.parse(os.getenv("DATE").strip('"'))
+        date = os.getenv("DATE").strip('"')
         mail = os.getenv("DEST").strip('"')
         body = os.getenv("BODY").strip('"')
         subject = os.getenv("SUBJECT").strip('"')
